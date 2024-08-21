c27495acfb84d6a19703258c0e70f1358496b6b1
Shaburdhin <smshaburdhinsmshaburdhin@gmail.com>
2024-08-21 16:52:45 +0530

    adding files

---
+++ node.js
@@ -0,0 +1,156 @@
+
+
+// let season = "summer"
+
+// if(season=="spring"){
+//   console.log("enjoy the blooming flowers")
+// }else if(season=="summer"){
+//     console.log("have fun in the sun")
+// }else if(season=="autumn"){
+//     console.log("admire the coloful leaves")
+// }else{
+//     console.log("bundle up and stay warm")
+// }
+
+// let color = "yellw"
+
+// if(color==="green"){
+//     console.log("go");
+// }else if(color==="red"){
+//     console.log("stop");
+// }else if(color==="yellow"){
+//     console.log("get ready");
+// }else{
+//     console.log("invalid input")
+// }
+
+// console.log(true&&true)
+// console.log(false&&true)
+
+//logical or
+
+// console.log(true||false)
+// console.log(true||true)
+// console.log(false||false)
+
+// let a = 100
+// let b = 30
+
+// if(a>b){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// var answer = true
+
+// if(answer){
+//     console.log("Great job")
+// }else{
+//     console,log("finish your homework before going to play")
+// }
+
+// if(answer){
+//     console.log("would you like a cookie")
+// }else{
+//     console.log("time to bake more cookies")
+// }
+
+// if(answer){
+//     console.log("you teeth are clean")
+// }else{
+//     console.log("go brush your teeth now")
+// }
+
+// if (answer){
+//     console.log("happy birthday! enjoy the day")
+// }else{
+//     console.log("have a great day ahead")
+// }
+
+// if(rain){
+//     console.log("take an umbrella")
+// }else{
+//     console.log("enjoy the sunshine")
+// }
+
+
+// if(true){
+//     console.log(true)
+    
+// }else{
+//     console.log(false)
+// }
+
+
+
+// function myname(){
+//     console.log("shabu")
+//     console.log("siva")
+//     return "shaburdhin"
+    
+// }
+
+// var a = myname()
+
+// console.log(a)
+
+
+// function area(l,b){
+//     var a = l*b
+//     console.log("the area is "+a)
+// }
+
+// area(10,20)
+
+// var factor = "thalapathy vijay"
+// var fplayer = "ms dhoni"
+// var fmovie = "leo"
+
+// console.log("my favourite movie"+fmovie)
+// console.log("my favourite actor"+factor)
+// console.log("my favourite player "+fplayer)
+
+// function oppo(){
+//     console.log("this is my oppo mobile")
+// }
+
+// oppo()
+
+// function redmi(){
+//     console.log("thi is my mi mobile")
+// }
+
+// function pocox2(){
+//     console.log("this is my pococ x2 mobile")
+// }
+ 
+ 
+// redmi()
+// pocox2()
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+// a = 0
+// b = 0
+// function adds(a,b){
+    
+//     c = a+b
+//     console.log(c)
+    
+// }
+
+// add(2,4)
+
+// adds(41,4)
+
+
+//  var a = 10 
+// var b = 10 
+// function multiply(a,b){
+//     c = a*b
+//     console.log(c)
+// }
+
+// multiply()
